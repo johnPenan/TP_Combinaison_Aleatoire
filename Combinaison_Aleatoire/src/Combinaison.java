b2d2d76da6973de4e109c73ff2705f4c1eb3c05a
Jean Penan <jeanpenangoumou1@gmail.com>
2018-05-14 22:53:42 +0200

    Ajout dans gitignore le fichier .recommenders.

--- Combinaison_Aleatoire/src/Combinaison.java
+++ Combinaison_Aleatoire/src/Combinaison.java
@@ -1,8 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Combinaison {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random rand = new Random();
+		int nbreSecret = rand.nextInt(100 + 1), maCombinaison, essai = 0;
+		Scanner sc = new Scanner(System.in);
+		char reponse = ' ';
+
+		do {
+			System.out.println(nbreSecret);
+			System.out.println("Veillez trouver le nombre secret:");
+			maCombinaison = sc.nextInt();
+			sc.nextLine();
+
+		} while (reponse == 'O');
 
 	}
 
